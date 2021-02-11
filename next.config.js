6397fe2f4fe867bc0860836aac731d9257794f35
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-02-11 13:46:11 -0800

    add all korean locales to next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: ['en-US', 'ko-KR'],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: 'en-US',
-      // This is a list of locale domains and the default locale they
-      // should handle (these are only required when setting up domain routing)
-    },
-  }
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en-US", "ko", "ko-kr", "ko-KR"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en-US",
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+  },
+};
